80048a8fffbf2f71691be9e6ce32ce7b70890332
Shivam Barthwal <shivambarthwal@live.com>
2024-01-11 22:36:07 +0100

    Comments Added on debounced

--- src/hooks/useDebouncedSearch.ts
+++ src/hooks/useDebouncedSearch.ts
@@ -2,6 +2,14 @@ import { useCallback } from "react";
 import { searchLaunches } from "../services/LaunchService";
 import debounce from 'lodash.debounce';
 
+/**
+ * Custom hook that provides a debounced search function and a cancel function.
+ * The debounced search function delays the execution of the search function until a certain delay has passed since the last call.
+ * The cancel function can be used to cancel the execution of the debounced search function.
+ * 
+ * @param delay The delay in milliseconds before executing the search function.
+ * @returns An object containing the debounced search function and the cancel function.
+ */
 export const useDebouncedSearch = (delay: number) => {
     const debouncedFunction = useCallback(debounce(async (query: string) => {
         return searchLaunches(query);
